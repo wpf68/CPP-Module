f959bf7f2a4d4b470e8cecdbefeb4c5d538d073d
Pascal <pascalwp68f@gmail.com>
2022-07-27 09:42:44 +0200

    220727_01

--- Module_00/ex00/megaphone.cpp
+++ Module_00/ex00/megaphone.cpp
@@ -12,7 +12,30 @@
 
 
 #include <iostream>
+//#include <cctype>
 
+int	main(int nb_arg, char **arg)
+{
+	int	i;
+	int	j;
+
+	if (nb_arg <= 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else 
+	{
+		i = 0;
+		while (++i < nb_arg)
+		{
+			j = -1;
+			while (arg[i][++j])
+				std::cout << (char)(toupper(arg[i][j]));
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
+
+/*
 void	ft_toupper(char	*arg)
 {
 	int	i;
@@ -22,27 +45,4 @@ void	ft_toupper(char	*arg)
 		if (arg[i] >= 'a' && arg[i] <= 'z')
 			arg[i] = arg[i] - 'a' + 'A';
 }
-
-int	main(int nb_arg, char **arg)
-{
-	int	i;
-
-	if (nb_arg <= 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	else 
-	{
-		i = 0;
-		while (++i < nb_arg)
-		{
-			ft_toupper(arg[i]);
-			std::cout << arg[i];
-		}
-		std::cout << std::endl;
-	}
-	return (0);
-}
-
-/* 
- * std::cout << (char)toupper(arg[i][j]);
- *
- */
+*/
